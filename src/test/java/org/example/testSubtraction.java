07425c84758cfade9e57a6c12b5c7ac4ed2db102
zhangxixi844 <1310335928@qq.com>
2024-09-25 18:04:10 +0800

    Add new test for subtraction method

---
+++ src/test/java/org/example/testSubtraction.java
@@ -0,0 +1,11 @@
+package org.example;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class testSubtraction {
+    //Calc c = new Calc();
+    @Test
+    void subtraction() {
+        assertEquals(2, )
+    }
+}
